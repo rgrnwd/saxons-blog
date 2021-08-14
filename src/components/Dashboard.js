cd13d25b1e4337792ea6e56fe2684e3f7e45c049
rgrnwd <rgrnwd@thoughtworks.com>
2021-08-14 15:38:59 +1000

    Render blog posts from sanity

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,5 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import sanityClient from "../client.js";
 
 export default function Dashboard() {
-  return <h2>Dashboard Page</h2>;
+  const [posts, setPosts] = useState(null);
+
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[_type == "post"]{
+        title,
+        slug,
+        mainImage{
+          asset->{
+          _id,
+          url
+        }
+      }
+    }`
+      )
+      .then((data) => setPosts(data))
+      .catch(console.error);
+  }, []);
+
+
+  return (
+    <div>
+      <h2>Welcome to Mancheeni!</h2>
+      <div>
+        {posts &&
+          posts.map((post, index) => (
+            <Link to={"/" + post.slug.current} key={post.slug.current}>
+              <span key={index}>
+                {post.mainImage&&<img src={post.mainImage.asset.url} alt="" />}
+                <span>
+                  <h2>{post.title}</h2>
+                </span>
+              </span>
+            </Link>
+          ))}
+      </div>
+    </div>
+  )
 }
