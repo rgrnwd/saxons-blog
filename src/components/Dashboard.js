955b83a89f7315ed600e0ab1e0c96ca2e0587fed
rgrnwd <rgrnwd@thoughtworks.com>
2021-08-14 17:50:37 +1000

    Style things a bit

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -25,20 +25,24 @@ export default function Dashboard() {
 
 
   return (
-    <div>
-      <h2>Welcome to Mancheeni!</h2>
-      <div>
-        {posts &&
-          posts.map((post, index) => (
-            <Link to={"/" + post.slug.current} key={post.slug.current}>
-              <span key={index}>
-                {post.mainImage&&<img src={post.mainImage.asset.url} alt="" />}
-                <span>
-                  <h2>{post.title}</h2>
+    <div className="bg-green-100 min-h-screen p-12">
+      <div className="container mx-auto">
+        <h2 className="text-5xl flex justify-center cursive">Welcome to Mancheeni!</h2>
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+          {posts &&
+            posts.map((post, index) => (
+              <Link to={"/" + post.slug.current} key={post.slug.current}>
+                <span className="block h-64 relative rounded shadow leading-snug bg-white border-l-8 border-green-400" key={index}>
+                  {post.mainImage && <img className="w-full h-full rounded-r object-cover absolute" src={post.mainImage.asset.url} /> }
+                  <span className="block relative h-full flex justify-end items-end pr -4 pb-4">
+                    <h2 className="text-gray-800 text-lg font-bold px-3 py-4 bg-red-700 text-red-100 bg-opacity-75 rounded">
+                      {post.title}
+                    </h2>
+                  </span>
                 </span>
-              </span>
-            </Link>
-          ))}
+              </Link>
+            ))}
+        </div>
       </div>
     </div>
   )
--- src/components/Post.js
+++ src/components/Post.js
@@ -38,22 +38,33 @@ export default function Post() {
   if (!postData) return <div>Loading...</div>;
 
   return (
-    <div>
-      <div>
-        <h2>{postData.title}</h2>
-        <div>
-          <h4>{postData.name}</h4>
+    <div className="bg-gray-200 min-h-screen p-12">
+      <div className="container shadow-lg mx-auto bg-green-100 rounded-lg">
+        <div className="relative">
+          <div className="absolute h-full w-full flex items-center justify-center p-8">
+            {/* Title Section */}
+            <div className="bg-white bg-opacity-75 rounded p-12">
+              <h2 className="cursive text-3xl lg:text-6xl mb-4">
+                {postData.title}
+              </h2>
+              <div className="flex justify-center text-gray-800">
+                {postData.authorImage&&<img src={urlFor(postData.authorImage).url()} className="w-10 h-10 rounded-full" />}
+                <h4 className="cursive flex items-center pl-2 text-2xl">
+                  {postData.name}
+                </h4>
+              </div>
+            </div>
+          </div>
+          { postData.mainImage && <img className="w-full object-cover rounded-t" src={urlFor(postData.mainImage).url()} style={{ height: "400px" }} /> }
         </div>
-      </div>
-      { postData.mainImage && <img src={urlFor(postData.mainImage).width(200).url()} /> }
-      <div>
-        <BlockContent
-          blocks={postData.body}
-          projectId={sanityClient.clientConfig.projectId}
-          dataset={sanityClient.clientConfig.dataset}
-        />
+        <div className="px-16 lg:px-48 py-12 lg:py-20 prose lg:prose-xl max-w-full">
+          <BlockContent
+            blocks={postData.body}
+            projectId={sanityClient.clientConfig.projectId}
+            dataset={sanityClient.clientConfig.dataset}
+          />
+        </div>
       </div>
     </div>
   );
-
 }
